d6a16b9c65935587d111d3eeca5af947af876c72
Mohammed Jewel <jewelnabin@gmail.com>
2022-05-06 02:06:56 +0600

    server side setting and running or test

---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors');
+const res = require('express/lib/response');
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@sparkles0.xtmyy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const itemCollection = client.db('sparklesUser').collection('item');
+        // const orderCollection = client.db('geniusCar').collection('order');
+
+        app.get('/item', async (req, res) => {
+            const query = {};
+            const cursor = itemCollection.find(query);
+            const items = await cursor.toArray();
+            res.send(items);
+        });
+
+        app.get('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const item = await itemCollection.findOne(query);
+            res.send(item);
+        });
+
+        // POST 
+        app.post('/item', async (req, res) => {
+            const newitem = req.body;
+            const result = await itemCollection.insertOne(newitem);
+            res.send(result);
+        });
+
+        // DELETE
+        app.delete('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await itemCollection.deleteOne(query);
+            res.send(result);
+        });
+
+        // Order Clollection API
+
+        app.get('/order', async (req, res) => {
+            const query = {};
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        })
+
+
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        })
+
+    }
+    finally {
+
+    }
+
+    console.log("Jewels Spareckes Connected");
+};
+
+run().catch(console.dir);
+
+// root API
+app.get('/', (req, res) => {
+    res.send('Running Jewels Sparkles');
+});
+
+app.listen(port, () => {
+    console.log('Listening to port', port);
+})
